61a4337a55d286ff77bebd93dd3dabb731962019
Dean Radcliffe <deanmisc@yahoo.com>
2015-08-30 21:53:09 -0500

    Include a self-linked document - W3C' links

--- examples/flow-router-example/client/router.js
+++ examples/flow-router-example/client/router.js
@@ -4,6 +4,12 @@ FlowRouter.route('/', {
   }
 });
 
+FlowRouter.route('/links', {
+  action: function() {
+    BlazeLayout.render("mainLayout", {content: "links"});
+  }
+});
+
 FlowRouter.route('/:postId', {
   action: function() {
     BlazeLayout.render("mainLayout", {content: "blogPost"});
