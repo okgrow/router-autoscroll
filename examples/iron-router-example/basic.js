e0c04b99ed4ee0bac0fee7d42a708f2887616294
Dean Radcliffe <deanmisc@yahoo.com>
2015-08-30 19:45:19 -0500

    template differences

--- examples/iron-router-example/basic.js
+++ examples/iron-router-example/basic.js
@@ -1,6 +1,6 @@
 Router.route('/', function () {
   // render the Home template with a custom data context
-  this.render('Home', {data: {title: 'My Title'}});
+  this.render('Home');
 });
 
 // when you navigate to "/one" automatically render the template named "One".
