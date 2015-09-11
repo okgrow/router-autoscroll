59c85bdc6813d102f519d589d066d73e9f2af778
Dean Radcliffe <deanmisc@yahoo.com>
2015-09-11 12:53:40 -0500

    example app testing the deprecation of iron-router-autoscroll

---
+++ examples/deprecated-iron-router-example/routes.js
@@ -0,0 +1,17 @@
+if (Meteor.isClient) {
+  IronRouterAutoscroll.animationDuration = 100;
+}
+
+Router.route('/', function () {
+  // render the Home template with a custom data context
+  this.render('Home');
+});
+
+// when you navigate to "/one" automatically render the template named "One".
+Router.route('/one');
+
+// when you navigate to "/two" automatically render the template named "Two".
+Router.route('/two');
+
+Router.route('/links');
+Router.route('/tables');
