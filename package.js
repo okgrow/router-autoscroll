a6da087cc77fb424491d4a54cb384829dabbd57f
Karl Danninger <kdanninger@gmail.com>
2015-11-16 11:49:31 -0500

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'okgrow:router-autoscroll',
-  version: '0.1.3',
+  version: '0.1.4',
   summary: 'Smart management of scroll position across route changes for Iron and Flow Router',
   git: 'https://github.com/okgrow/router-autoscroll',
   documentation: 'README.md'
