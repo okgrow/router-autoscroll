acb282cd6f0a5f1f5e052e5085c92c82f2ed7cee
Ben Rothman <bensrothman@gmail.com>
2015-11-24 11:52:45 -0600

    Add nightwatch automation tests

---
+++ tests/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  servers: {
+    'iron-router': 'localhost:3120/one',
+    'flow-router': 'localhost:4160'
+  },
+  viewport: {
+    width: 1000,
+    height: 500
+  },
+  timeout: 3000,
+  animationDuration: 300
+};
